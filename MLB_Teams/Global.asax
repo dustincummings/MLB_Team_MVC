924bcc08b6cdcde5477fa8451a5074e048a6563e
Dustin Cummings <dustincummings15@gmail.com>
2018-11-20 14:10:37 -0500

    Add project files.

---
+++ MLB_Teams/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MLB_Teams.MvcApplication" Language="C#" %>
---
+++ MLB_Teams/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MLB_Teams.Startup))]
+namespace MLB_Teams
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
